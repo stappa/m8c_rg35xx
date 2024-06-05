553ddf63ae3b08de4a9603875b22968dd21145b2
Pavel Tatarinov <ptatarinov@elvees.com>
2024-06-05 18:53:39 +0600

    Fix config dir creation

--- scripts/install_m8.sh
+++ scripts/install_m8.sh
@@ -7,6 +7,7 @@ cp ./scripts/m8c.sh /userdata/system/.simplemenu/games/
 mkdir -p /userdata/system/.simplemenu/games/Imgs
 cp ./imgs/m8c.png /userdata/system/.simplemenu/games/Imgs/
 cp ./scripts/m8c.sh /userdata/system/.simplemenu/games/
+mkdir -p /userdata/system/.local/share/m8c
 cp ./config/config.ini /userdata/system/.local/share/m8c/
 
 mkdir -p /lib/modules/3.10.37/kernel/sound/core
